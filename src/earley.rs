99e6380cf6d8af78f990e501aa4b8e511783c975
Hudson Cooper <hncooper96@gmail.com>
2024-03-17 14:10:04 -0700

    improve(🤞) ownerships

--- src/earley.rs
+++ src/earley.rs
@@ -1,28 +1,37 @@
 use std::fmt;
 
-enum Symbol<'a> {
+enum Symbol {
     Terminal(Vec<char>),
-    Nonterminal(&'a Nonterminal),
-}
-
-struct Nonterminal {
-    name: String,
+    Nonterminal { name: String },
 }
 
 struct Production<'a> {
-    nonterminal: &'a Nonterminal,
-    symbols: Vec<Symbol<'a>>,
+    nonterminal: &'a Symbol, //TODO: type-narrow??
+    symbols: Vec<&'a Symbol>,
 }
 
 struct EarleyItem<'a> {
     pos: usize,
     start: usize,
-    production: Production<'a>,
+    production: &'a Production<'a>,
+}
+
+struct Grammar<'a> {
+    productions: Vec<Production<'a>>,
+}
+
+impl fmt::Display for Grammar<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for prod in self.productions.iter() {
+            writeln!(f, "{}", prod)?;
+        }
+        write!(f, "")
+    }
 }
 
 impl fmt::Display for Production<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} ->", self.nonterminal.name)?; // TODO: implement nonterminal fmt::Display?
+        write!(f, "{} ->", self.nonterminal)?;
         for symbol in self.symbols.iter() {
             write!(f, " {}", symbol)?;
         }
@@ -30,18 +39,22 @@ impl fmt::Display for Production<'_> {
     }
 }
 
-impl fmt::Display for Symbol<'_> {
+impl fmt::Display for Symbol {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Symbol::Terminal(chars) => {
-                if chars.len() > 1 { write!(f, "[")?; }
-                for ch in chars {
+                if chars.len() > 1 {
+                    write!(f, "[")?;
+                }
+                for ch in chars.iter() {
                     write!(f, "{}", ch)?;
                 }
-                if chars.len() > 1 { write!(f, "]")?; }
+                if chars.len() > 1 {
+                    write!(f, "]")?;
+                }
                 write!(f, "")
             }
-            Symbol::Nonterminal(Nonterminal { name, .. }) => write!(f, "{}", name),
+            Symbol::Nonterminal { name, .. } => write!(f, "{}", name),
         }
     }
 }
@@ -49,7 +62,7 @@ impl fmt::Display for Symbol<'_> {
 impl fmt::Display for EarleyItem<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let prod = &self.production;
-        write!(f, "{} ->", prod.nonterminal.name)?;
+        write!(f, "{} ->", prod.nonterminal)?;
         for (i, symbol) in prod.symbols.iter().enumerate() {
             if i == self.pos {
                 write!(f, " •")?;
@@ -64,18 +77,21 @@ impl fmt::Display for EarleyItem<'_> {
 }
 
 pub fn main() {
-    let number = Nonterminal {
+    let number = Symbol::Nonterminal {
         name: "Number".to_string(),
     };
-    let prod = Production {
-        nonterminal: &number,
-        symbols: vec![Symbol::Nonterminal(&number), Symbol::Terminal(('0'..='9').collect())],
+    let digit = Symbol::Terminal(('0'..='9').collect());
+    let grammar = Grammar {
+        productions: vec![
+            Production {
+                nonterminal: &number,
+                symbols: vec![&number, &digit],
+            },
+            Production {
+                nonterminal: &number,
+                symbols: vec![&digit],
+            },
+        ],
     };
-    println!("{}", prod);
-    let ear: EarleyItem = EarleyItem {
-        pos: 1,
-        start: 1,
-        production: prod,
-    };
-    println!("{}", ear);
+    print!("{}", grammar);
 }
