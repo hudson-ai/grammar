b76dca338077340c90d1a8d74910dab5b676fa70
Hudson Cooper <hncooper96@gmail.com>
2024-03-16 22:48:02 -0700

    earleyitem (well, just its repr)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
-mod tok;
+use std::fmt;
+
+#[derive(Debug)]
+enum Symbol {
+    Terminal(char),
+    Nonterminal,
+}
+
+struct EarleyItem {
+    pos: usize,
+    start: usize,
+    lhs: Symbol,
+    rhs: Vec<Symbol>,
+}
+
+impl fmt::Display for EarleyItem {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?} ->", self.lhs)?;
+        for (i, item) in self.rhs.iter().enumerate() {
+            if i == self.pos {
+                write!(f, " •")?;
+            };
+            write!(f, " {:?}", item)?;
+        }
+        if self.pos == self.rhs.len() {
+            write!(f, " •")?;
+        }
+        write!(f, " ({})", self.start)
+    }
+}
 
 fn main() {
-    let string = "3 + 4 + (5 + 6)";
-    let tokens = tok::tokenize(string);
-    for token in tokens {
-        println!("{:?}", token)
-    }
+    let ear = EarleyItem {
+        pos: 1,
+        start: 1,
+        lhs: Symbol::Nonterminal,
+        rhs: vec![Symbol::Terminal('a'), Symbol::Terminal('b')],
+    };
+    println!("{}", ear)
 }
